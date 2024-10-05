e4fa4200f1d4b9faf80875f76b4069cdd815da87
AnandAnasane <aanand08a@gmail.com>
2024-10-05 17:10:58 +0000

    kms conversion

---
+++ tasks/06_km_conversion.js
@@ -0,0 +1,7 @@
+// To convert km into miles or miles into kms
+// 1 km = 0.621371 miles
+let kms = 10;
+const factor = 0.621371
+const miles = kms *factor;
+
+console.log(`conversion of km ${kms} into miles is ${miles}`);
