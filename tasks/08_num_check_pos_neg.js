62507d506a411f974bd171e9fbef0929e0556d29
AnandAnasane <aanand08a@gmail.com>
2024-10-09 11:13:55 +0000

    update

---
+++ tasks/08_num_check_pos_neg.js
@@ -0,0 +1,18 @@
+// WAP to check the number is positive or Negative
+
+// With using the Math.sign() Method
+
+const a = 0;
+const result = Math.sign(a);
+console.log(result);
+
+//Output 1 = Positive
+//Output 2 = Negative
+//Output 0 = Zero
+
+
+
+// second withouth using Math.sign() method
+const num1 = -30;
+if(num1 >= 0) console.log(`${num1} is a positive number`);
+else console.log(`${num1} is a Negative number`);
