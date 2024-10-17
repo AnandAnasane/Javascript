d649d38010ab74ee2fef4a85f119747af0250cbc
AnandAnasane <aanand08a@gmail.com>
2024-10-17 04:20:49 +0000

    modified positive negative number file

---
+++ tasks/09_pos_neg_num.js
@@ -0,0 +1,18 @@
+// Check number is positive or Negative
+
+//  1. with if and else
+
+const num = 45 ;
+
+if(num%2 == 0) console.log(`${num}  is a positive number`);
+else console.log(`${num} is a Negative number`);
+
+
+// with ternary operator
+
+const a = 110;
+const result = (a%2==0) ? `${a} is a positive number` : `${a} is a Negative number`;
+// condition ? true : false ;
+console.log(result);
+
+
