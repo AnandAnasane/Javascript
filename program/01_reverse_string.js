1961667940d20b0da7826ffc23208786cc0a37b7
AnandAnasane <aanand08a@gmail.com>
2024-12-05 17:45:51 +0000

    update

--- program/01_reverse_string.js
+++ program/01_reverse_string.js
@@ -1,20 +1,8 @@
-function reverseString(str){
-    let reversed = '';
-    
-    for(let i = str.length-1; i>=0; i--){
-
+function reverseStr(str){
+    let reversed = "";
+    for(let i = str.length - 1; i>=0; i--){
         reversed += str[i];
     }
-    return reversed
-
+    return reversed ;
 }
-console.log(reverseString('Hello'));
-
-
-
-// With pre-defined methods
-function reverseString(str) {
-    return str.split('').reverse().join('');
-}
-console.log(reverseString("hello"));  // Output: "olleh"
-
+console.log(reverseStr("hello"));
