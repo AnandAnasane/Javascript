5ea1ce84b3fffad7a5333bca5ff90d67a0be4c10
AnandAnasane <aanand08a@gmail.com>
2024-12-08 14:27:36 +0000

    update compairision

---
+++ topics/compair.js
@@ -0,0 +1,6 @@
+// == checks for equality after type coercion.
+// === checks for equality without type coercion (strict equality).
+
+console.log(5 == '5'); // true (type coercion)
+console.log(5 === '5');  // false (strict comparison)
+
