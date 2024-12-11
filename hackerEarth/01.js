f89e60710fae71a5d7c5e010af45585fe8be8332
AnandAnasane <aanand08a@gmail.com>
2024-12-11 05:39:35 +0000

    updated

--- hackerEarth/01.js
+++ hackerEarth/01.js
@@ -1,23 +1,3 @@
-// const arr = [1,1,2,2,4];
-// let count = 0;
-
-// function countOccurance(arr){
-    
-
-//     for(let i=0; i<=arr.length; i++){
-//         for(let j=arr.length; j>=0; j--){
-//             if(arr[i]===arr[j]){
-//                 count++;
-//             }
-//         }
-        
-//     }
-//     return count;
-   
-// }
-// console.log(count);
-
-
 
 function countUniqueRepeatedElements(arr) {
     let seen = {}; 
