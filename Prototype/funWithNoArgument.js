5e4efc51dd4c6fcdd72de4a3b463f06fc24463dc
AnandAnasane <aanand08a@gmail.com>
2025-04-10 11:11:34 +0000

    update prototype

--- Prototype/funWithNoArgument.js
+++ Prototype/funWithNoArgument.js
@@ -9,3 +9,25 @@ printDetails.prototype.printName = function (){
 const Car = new printDetails("Audi")
 Car.printName();
 
+
+
+///////////////////////
+
+// Prototype with Object
+
+var student = {
+    name: 'Anand',
+    city: 'Pune',
+    printName: function(){
+        console.log('MyName is' + this.name)
+    }
+
+}
+
+let Student2 = {
+    name: 'Rahul',
+    city: 'Banglore'
+}
+
+Student2.__proto__ = student
+Student2.printName();
