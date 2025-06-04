1799d99496781718cad70a48e70555efc78920a8
AnandAnasane <aanand08a@gmail.com>
2025-06-04 14:50:19 +0000

    a

---
+++ practice.js
@@ -0,0 +1,50 @@
+// Fibnoacci Series
+//  fibnoacci =  (n) => {
+//     let a = 0;
+//     let b = 1;
+
+//     for (let i = 0; i<= n ; i++){
+//         console.log(a);
+//         let next = a + b; 
+//         a = b; 
+//         b = next;
+//     }
+// }
+// fibnoacci(10)
+
+
+
+// reverse an array
+
+// const arr = [ 1,2,3,4,5]
+// const reversedArray = [ ];
+// for(let i = arr.length-1; i>= 0; i--){
+//     reversedArray.push(arr[i])
+// }
+// console.log(reversedArray)
+
+// print duplicate array elements
+// const arr = [ 1,1,2,2,3,3,4,4,5,5,6,7,8,9,0]
+// const duplicate = [ ];
+
+// for(let i = 0; i<arr.length; i++){
+//     for(let j = i + 1; j<arr.length; j++){
+
+//         if(arr[i] === arr[j] && !duplicate.includes(arr[i])){
+//             duplicate.push(arr[i])
+//         }
+//     }
+// }
+// console.log(duplicate)
+
+
+// remove duplicate array elements print remaining
+
+// const arr = [ 1,1,2,2,3,3,4,4,5,5,6,6,7,8,9,0]
+// const remainingArray = []
+// for(let i = 0; i<= arr.length-1; i++){
+//     if(!remainingArray.includes(arr[i])){
+//         remainingArray.push(arr[i]);
+//     }
+// }
+// console.log(remainingArray)
