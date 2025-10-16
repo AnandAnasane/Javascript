4fdf685d6167ca2cd7e3aefaea1ce88039b18201
AnandAnasane <aanand08a@gmail.com>
2025-10-16 02:16:01 +0000

    commit chnges

--- array/second_largest_no.js
+++ array/second_largest_no.js
@@ -1,15 +1,21 @@
-function findSecondLargest(arr) {
+function findSecondLargest(arr)
+{
     let largest = -Infinity, secondLargest = -Infinity;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > largest) {
+
+    for(let i = 0; i < arr.length; i++){
+
+        if(arr[i] > largest){
             secondLargest = largest;
-            largest = arr[i];
-        } else if (arr[i] > secondLargest && arr[i] !== largest) {
-            secondLargest = arr[i];
+            largest = arr[i]
         }
+        else if(arr[i] > secondLargest && arr[i] !== largest)
+            {
+                secondLargest = arr[i];
+        }
+
     }
     return secondLargest;
-}
 
-let numbers = [10, 5, 8, 20, 15];
-console.log(findSecondLargest(numbers));
+}
+let num = [11,22,33,44,55,66];
+console.log(findSecondLargest(num));
